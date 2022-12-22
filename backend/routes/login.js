9bb340700e86a509fd14f6fb7edd4f9378f980bf
withydn <withydn@naver.com>
2022-12-22 21:50:31 +0900

    a1222
    a1222

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,16 +1,18 @@
+// @ts-check
+
 const express = require("express");
 const router = express.Router();
 const db = require("../controllers/mongoController");
 
+// 로그인 페이지
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// 로그인 연결
 router.post("/login", async (req, res) => {
   const loginInfo = req.body;
   const result = await db.login(loginInfo);
-  res.send(JSON.stringify(result));
-});
-
-router.post("/register", async (req, res) => {
-  const registerInfo = req.body;
-  const result = await db.register(registerInfo);
   res.send(JSON.stringify(result));
 });
 
--- src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import users from "./modules/users";
-
-export default combineReducers({
-  users,
-});
