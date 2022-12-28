4d30f9c93a210c98b1d41ed7f942cf888c02c9c6
2Sodol <104712554+dex7688@users.noreply.github.com>
2022-12-28 22:29:29 +0900

    wooseok_12_28

---
+++ backend/routes/likes.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+const express = require('express');
+const router = express.Router();
+const db = require('../controllers/userController');
+
+router.post('/', async (req, res) => {
+  const result = await db.addLike(req.body);
+  res.send(JSON.stringify(result));
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 // 필요 모듈 설정
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 const PORT = 4500;
 
@@ -10,13 +10,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-const loginRouter = require("./routes/login");
-const signupRouter = require("./routes/register");
-const reviewRouter = require("./routes/review");
+const loginRouter = require('./routes/login');
+const signupRouter = require('./routes/register');
+const reviewRouter = require('./routes/review');
+const addLikeRouter = require('./routes/likes');
 
-app.use("/login", loginRouter);
-app.use("/register", signupRouter);
-app.use("/review", reviewRouter);
+app.use('/login', loginRouter);
+app.use('/register', signupRouter);
+app.use('/review', reviewRouter);
+app.use('/addLike', addLikeRouter);
 
 app.listen(PORT, () => {
   console.log(`데이터 통신 서버가 ${PORT}에서 작동 중입니다!`);
